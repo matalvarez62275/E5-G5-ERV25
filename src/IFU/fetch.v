7d9a1cc7dabf5fa2c44993ab9c6fba6c91a2a554
matalvarez62275 <matalvarez@itba.edu.ar>
2025-05-03 19:33:21 -0300

    IFU Update

--- src/IFU/fetch.v
+++ src/IFU/fetch.v
@@ -9,13 +9,8 @@ output[31:0] nxt_PC
 	reg[31:0] programcounter;
 
 	always @(posedge clk or negedge nreset) begin
-		// Reset logic
-		if(!nreset) 
-			programcounter <= 0;
-		
-		// Clcok gating
-		else if(en)
-			programcounter <= curr_PC + 4; // Byte indexed 32 bit address
+		if(!nreset) programcounter <= 0;
+		else if(en) programcounter <= curr_PC + 4;
 	end
 	
 	assign nxt_PC = programcounter;
