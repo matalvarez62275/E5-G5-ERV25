f7f05d8ee792109b538c1562055f30a44e791e44
matalvarez62275 <matalvarez@itba.edu.ar>
2025-05-03 10:26:26 -0300

    Rollback safe point

--- src/IFU/fetch.v
+++ src/IFU/fetch.v
@@ -9,8 +9,13 @@ output[31:0] nxt_PC
 	reg[31:0] programcounter;
 
 	always @(posedge clk or negedge nreset) begin
-		if(!nreset) programcounter <= 0;
-		else if(en) programcounter <= curr_PC + 4;
+		// Reset logic
+		if(!nreset) 
+			programcounter <= 0;
+		
+		// Clcok gating
+		else if(en)
+			programcounter <= curr_PC + 4; // Byte indexed 32 bit address
 	end
 	
 	assign nxt_PC = programcounter;
