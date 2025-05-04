62a386d687586ccd3d4000371fffc6aaa41967d9
juan-mar <juanmrodriguez@itba.edu.ar>
2025-05-04 18:08:22 -0300

    avances opcode

---
+++ project/Opcode-Decode.sv
@@ -0,0 +1,67 @@
+module Opcode-Decode (
+   input wire[31:0] inst,
+	
+	output wire[4:0] rd,
+	output wire[4:0] rs1,
+	output wire[4:0] rs2,
+	output wire[2:0] funct3,
+
+	output reg rd_en,		
+	output reg rs1_en,	
+	output reg rs2_en,	
+	output reg imm_en,	
+	output reg ALU_en,
+	output reg func3_valid,
+);
+
+	assign rd = inst[11:7];
+	assign rs1 = inst[19:15];
+	assign rs2 = inst[24:20];
+	assign funct3 = inst[14:12];
+	
+	wire[4:0] opcode = inst[6:2];
+		
+	always @(opcode)
+	begin 
+		if(opcode == 5'b11011) begin //Tipo J, inst unica JAL
+			rd_en = 1;
+		end else if (opcode == 5'b01101 || opcode == 5'b00101) begin //Tipo U, inst unica JAL
+			rd_en = 1;
+		end else begin 
+		
+			case(opcode[6:4]) 
+				3'b000: begin 				 //Instruccion LOAD tipo I
+				
+				end
+				3'b001: begin			
+					if(funct3 == 3'b101 || funct3 == 3'b001 )	begin //Instruccion ALU tipo R
+						
+						
+					end else begin 		//Instruccion ALU tipo I														
+						
+						
+					end
+					
+				end
+				3'b011: begin				//Instruccion ALU tipo R		
+					
+				end
+				3'b010: begin 				//Instruccion Store tipo S
+				
+				end
+				
+				3'b110: begin 				//Instruccion Branch tipo B
+				
+				end
+				3'b111: begin 				//Instruccion CSR tipo I
+				
+				end	
+			end case
+		end 	
+	end
+	
+	
+
+	
+
+endmodule
